3b70df612fcd1d1ae37747f3bd333b843c55d3cc
Guel-git <eccyuna@gmail.com>
2023-03-02 00:36:46 +0900

    [ADD] CollectionView 구현(#1)

--- Wanted-Pre-Onboarding/Wanted-Pre-Onboarding/Screen/UIComponent/CollectionView.swift
+++ Wanted-Pre-Onboarding/Wanted-Pre-Onboarding/Screen/UIComponent/CollectionView.swift
@@ -7,6 +7,65 @@
 
 import UIKit
 
-final class CollectionView: UICollectionView {
+final class CollectionView: UIView {
     
+    private enum Size {
+        static let collectionHorizontalSpacing: CGFloat = 24
+        static let collectionVerticalSpacing: CGFloat = 16
+        static let cellWidth: CGFloat = UIScreen.main.bounds.width - 24 * 2
+        static let cellHeight: CGFloat = 32
+        static let collectionInsets = UIEdgeInsets(
+            top: collectionVerticalSpacing,
+            left: collectionHorizontalSpacing,
+            bottom: collectionVerticalSpacing,
+            right: collectionHorizontalSpacing)
+    }
+    
+    // MARK: - property
+    
+    private let collectionViewFlowLayout: UICollectionViewFlowLayout = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.sectionInset = Size.collectionInsets
+        flowLayout.itemSize = CGSize(width: Size.cellWidth, height: Size.cellHeight)
+        flowLayout.minimumLineSpacing = 24
+        return flowLayout
+    }()
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+        collectionView.backgroundColor = .clear
+        collectionView.dataSource = self
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        return collectionView
+    }()
+    
+    // MARK: - life cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
+    private func render() {
+        self.addSubview(collectionView)
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - extension
+
+extension CollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        return cell
+    }
 }
