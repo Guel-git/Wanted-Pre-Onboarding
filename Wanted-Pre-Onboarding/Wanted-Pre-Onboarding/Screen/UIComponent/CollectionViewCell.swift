e2702e3ca8d6f9bfc78ab81cadff21afbbd90459
Guel-git <eccyuna@gmail.com>
2023-03-01 23:52:44 +0900

    [ADD] CollectionView & CollectionViewCell 생성(#1)

---
+++ Wanted-Pre-Onboarding/Wanted-Pre-Onboarding/Screen/UIComponent/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Wanted-Pre-Onboarding
+//
+//  Created by 김유나 on 2023/03/01.
+//
+
+import UIKit
+
+final class CollectionViewCell: UICollectionViewCell {
+    
+}
